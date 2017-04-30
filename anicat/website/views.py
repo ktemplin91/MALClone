a800ca9528c393ca862f460dc0fb85458450baf0
Kevin Templin <kevin.templin91@gmail.com>
2017-04-30 07:33:35 -0500

    Fully Divided Register / Login Forms Modal
    Implemented bootstrap and css to divided the temporary register and
    login forms modal.

--- anicat/website/views.py
+++ anicat/website/views.py
@@ -3,13 +3,9 @@ from django.http import HttpResponse
 from website.forms import *
 # Create your views here.
 
-#def index(request):
-   #template = loader.get_template('website/base.html')
-   # return HttpResponse("Hello, world. You're at the polls index.")
- #  context ={
-
-  # }
-  # return render(request, "index.html",{})
+def index(request):
+    # This view is missing all form handling logic for simplicity of the example
+    return render(request, 'index.html', {'form': MessageForm()})
 
 def animepage(request):
 	context={
@@ -47,9 +43,3 @@ def helppage(request):
 	}
 	return render(request, "helppage.html", {})
 
-
-
-
-def index(request):
-    # This view is missing all form handling logic for simplicity of the example
-    return render(request, 'index.html', {'form': MessageForm()})
